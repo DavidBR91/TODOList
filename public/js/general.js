d76fcb4a56b4775901f981d8cdb70c204a29d529
Toni <a.rico.diez@gmail.com>
2012-11-20 23:39:31 +0100

    Nuevos cambios en la estructura y añadidos cambios en la apariencia de las listas de tareas

--- public/js/general.js
+++ public/js/general.js
@@ -11,9 +11,12 @@ function equalHeight(group) {
 	});
 }
 function changeList(list){
-	
+	$('.taskList').empty();
+	$('.taskListTitle').empty();
+	$('.taskListTitle').append('<h2>'+list+'</h2>');
 }
 $(document).ready(function() {
+	$('.listElement').first().parent().addClass('active');
 	equalHeight($(".taskRow .task"));
     $('#submitButton').on('click', function(e){
     	// We don't want this to act as a link so cancel the link action
@@ -21,7 +24,23 @@ $(document).ready(function() {
     	// Find form and submit it
     	$('.taskOptionsForm').submit();
     });
-    $("#dropdownLists").change(function() { 
-    	changeList($(this).val());
+    $('#dropdownLists').change(function() { 
+    	var list=$(this).val();
+    	var i=0;
+    	var dropdownIndex=$('#dropdownLists').get(0).selectedIndex;
+        $('#dropdownLists').parent().parent().find('.active').removeClass('active');
+        $('.todoLists > ul > li > a').each(function() {
+        	if(dropdownIndex==i++){
+        		$(this).parent().addClass('active');
+        	}
+        });
+        changeList(list);
+    });
+    $('.listElement').on('click',function(event) { 
+    	var list=$(this).text();
+    	$('#dropdownLists').val(list);
+    	$(this).parent().parent().find('.active').removeClass('active');
+    	$(this).parent().addClass('active');
+    	changeList(list);
     });
 });
