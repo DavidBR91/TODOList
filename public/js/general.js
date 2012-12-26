0a487ae32316abc77cc272a60c74b79128d56e5f
Toni <a.rico.diez@gmail.com>
2012-12-26 18:54:49 +0100

    Eliminado Dropdown menu para listas, Arreglado estado completado de tareas, División de javascript en ficheros models y views, primera inclusión de modelo de mapas de listas

--- public/js/general.js
+++ public/js/general.js
@@ -17,112 +17,16 @@ function changeList(list){
 	$('#taskList').empty();
 	$('#taskListTitle').empty();
 	$('#taskListTitle').append('<h1>'+list+'</h1><hr>');
-	$('#dropdownLists').val(list);
-	var i=0;
-    var dropdownIndex=$('#dropdownLists').get(0).selectedIndex;
 	$('.active').removeClass('active');
-	$('#dropdownLists').removeClass('active');
     $('.listElement').each(function() {
-    	if(dropdownIndex==i++){
+    	if($(this).text()==list){
     		$(this).parent().addClass('active');
     	}
     });
-    var modelTasks=lists.at($('#dropdownLists').get(0).selectedIndex).get('tasks');
+    var modelTasks=lists.at($('.active').index()/2).get('tasks');
     var tasksView=new TasksView({el: $('#taskList'), model: modelTasks});
     tasksView.render();
 }
-var List=Backbone.Model.extend({
-	idAttribute: '_id',
-	parse: function(response) {
-		var taskList=[];
-		this.url='/list/'+response._id;
-		_.each(response.tasks,function(taskid) {
-			var task=new Task({listid: response._id, _id: taskid});
-			task.fetch({
-				success: function(task) {}
-			});
-			taskList.push(task);
-		});
-		response.tasks=new TaskList(taskList);
-		response.tasks.setParams(response._id,this,response.user);
-		return response;
-	}
-});
-var Lists=Backbone.Collection.extend({
-	model: List,
-	url: '/list',
-	comparator: function(list) {
-		return list.name
-	}
-});
-var Task=Backbone.Model.extend({
-	idAttribute: '_id',
-	initialize: function(options) {
-		if(typeof options._id!='undefined'){
-			this.url='/list/'+options.listid+'/task/'+options._id;
-		}
-	},
-	defaults:{
-		name: '',
-		description: '',
-		expiration_date : new Date(),
-		expectedDays: 0,
-		completed: false
-	}
-});
-var TaskList=Backbone.Collection.extend({
-	model: Task,
-	setParams: function(listid,list,user) {
-		this.url='/list/'+listid+'/task';
-		this.list=list;
-		this.user=user;
-	},
-	comparator: function(task) {
-		//var date=new Date(task.get("limitYear"),task.get("limitMonth"),task.get("limitDay"),0,0,0,0);
-		//return date.getTime();
-		if(typeof task.expiration_date != 'undefined') return task.expiration_date.getTime();
-		else return task.name;
-	}
-});
-var ListsView=Backbone.View.extend({
-	initialize: function(){
-		this.model.bind('add',this.render,this);
-		this.model.bind('remove',this.render,this);
-    },
-    render: function(){
-    	var listNameArray=[];
-    	this.$el.empty();
-    	lists.each(function(list) {
-	    	listNameArray.push({name: list.get('name') });
-    	});
-        var template = _.template( $("#listsNavbar").html(), {lists: listNameArray} );
-        this.$el.append( template );
-        $('.listElement').on('click',function(event) {
-	    	var list=$(this).text();
-	    	$('#dropdownLists').val(list);
-	    	changeList(list);
-    	});
-    	$('#dropdownLists').change(function() {
-	    	var list=$(this).val();
-	        changeList(list);
-	    });
-	     $('#deleteList').on('click', function(e){
-	     	var list=lists.at($('#dropdownLists').get(0).selectedIndex);
-	    	lists.remove(list);
-	    	list.destroy();
-	    	changeList(lists.at(0).get('name'));
-	    });
-        //console.log($(template).find('input'));
-        var inputs = $(template).find('input');
-        for(var i =0; i < inputs.length; i++){
-            inputs.eq(i).change(handler)
-        }
-        $(template).find('input').click(handler);
-        //template.find('input').change(handler);
-
-        return this;
-    }
-});
 
 var handler = function(event) {
     var input = $(this);
@@ -144,71 +48,25 @@ var handler = function(event) {
 
 //$('.nav-list').find('input').change(handler);
 
-var inDes = $('#inputDescription');
-var inLimD = $('#inputLimitDay');
-var inLimM= $('#inputLimitMonth');
-var inLimY = $('#inputLimitYear');
-var inComp = $('#inputCompleted');
-var inExpD = $('#inputExpectedDays');
-
-var TasksView=Backbone.View.extend({
-	initialize: function(){
-		this.model.bind('add',this.render,this);
-		this.model.bind('remove',this.render,this);
-		this.model.bind('change',this.render,this);
-    },
-    render: function(){
-    	var taskArray=[];
-    	this.$el.empty();
-    	this.model.each(function(task) {
-    		/*if(task.get('limitDay')==0||task.get('limitMonth')==0||task.get('limitYear')==0){
-	    		taskArray.push({name: task.get('name'),limitDate: '', description : task.get('description')});
-    		}else{
-	    		taskArray.push({name: task.get('name'),limitDate: task.get('limitDay')+"/"+task.get('limitMonth')+"/"+task.get('limitYear'), description : task.get('description')});
-	    	}*/
-	    	taskArray.push({name: task.get('name'),limitDate: task.get('expiration_date'), completed : task.get('completed'), description : task.get('description')});
-    	});
-	    	var template = _.template( $("#taskRowTemplate").html(), {tasks: taskArray} );
-	    	this.$el.append( template );
-      $('[rel=tooltip]').tooltip();
-    	$('.task').click(function() {
-		    var taskName = $(this).data('name');
-		    $('#inputTitle').val(taskName);
-		    $('#taskOptionsLabel').text(taskName);
-		    var tasks=lists.at($('#dropdownLists').get(0).selectedIndex).get('tasks');
-		    tasks.each(function(task) {
-		    	if(task.get('name')===taskName){
-		    		var date=new Date(task.get('expiration_date'));
-			    	inDes.val(task.get('description'));
-			    	inLimD.val(date.getDate());
-			    	inLimM.val(date.getMonth()+1);
-			      inLimY.val(date.getFullYear());
-			    	inExpD.val(task.get('expectedDays'));
-			    	task.get('completed') ?  inComp.attr('checked', false) : inComp.attr('checked', true);
-		    	}
-		    });
-	    });
-    	return this;
-    }
-});
 $(document).ready(function() {
 	lists=new Lists();
 	lists.fetch({
 		success: function() {
 			listsView=new ListsView({el: $('#listsWell'), model: lists});
 			listsView.render();
+			listsView
             var inputs = $('.nav-list').find('input');
             inputs.each(function(i) {
                 inputs.eq(i).change(handler);
             });
+            $('.listElement').first().parent().addClass('active');
 			changeList(lists.at(0).get('name'));
-			$('.listElement').first().parent().addClass('active');
 			equalHeight($(".taskRow .task"));
 			$('#submitButton').on('click', function(e){
 				$('.taskOptionsForm').submit();	
 			});
 		    $('.taskOptionsForm').on('submit', function(e){
-		    	var tasks=lists.at($('#dropdownLists').get(0).selectedIndex).get('tasks');
+		    	var tasks=lists.at($('.active').index()/2).get('tasks');
 		    	tasks.each(function(task) {
 			    	if(task.get('name')==$('#taskOptionsLabel').text()){
 			    		//task.set({name: $('#inputTitle').val(),description: $('#inputDescription').val(),expiration_date: new Date($('#inputLimitYear').val(),$('#inputLimitMonth').val()-1,$('#inputLimitDay').val(),0,0,0,0),expectedDays: $('#inputExpectedDays').val(),completed: $('#inputCompleted').attr('checked')?true:false});
@@ -240,7 +98,7 @@ $(document).ready(function() {
 		    $('.newTaskForm').on('submit', function(e){
 		    	var newTaskName=$('#inputTaskName').val();
 		    	$('#inputTaskName').val('')
-		    	var selectedList=lists.at($('#dropdownLists').get(0).selectedIndex);
+		    	var selectedList=lists.at($('.active').index()/2);
 		    	var listName=selectedList.get('name');
 		    	var tasks=selectedList.get('tasks');
 		    	var newTask=new Task({listid: selectedList.get('_id'),name: newTaskName});
@@ -249,7 +107,7 @@ $(document).ready(function() {
 		    	$("#newTaskModal").modal('hide');
 		    });
 		    $('#removeButton').on('click', function(e){
-		    	var tasks=lists.at($('#dropdownLists').get(0).selectedIndex).get('tasks');
+		    	var tasks=lists.at($('.active').index()/2).get('tasks');
 		    	tasks.each(function(task) {
 			    	if(task.get('name')==$('#taskOptionsLabel').text()){
 			    		tasks.remove(task);
